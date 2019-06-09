4d373deee04b29651b8c1a0316552ba2eaf0cfd2
Owen <ojripley19@gmail.com>
2019-06-09 01:28:28 +0000

    added ice-cream.js

---
+++ ice-cream.js
@@ -0,0 +1,11 @@
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push("root beer");
+
+console.log(icecreamFlavours[0]);
+
+console.log(iceCreamFlavours[iceCreamFlavours.length-1]);
+
+console.log(iceCreamFLavours.length);
